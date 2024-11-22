2c5e5af06f2e6f5c7747e261b8014f6664991fba
Jack Greenwald <72763093+JGreenwald538@users.noreply.github.com>
2024-11-22 23:51:49 +0200

    Updated sidebar color

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({borderColor} : {borderColor: string}) {
+    const border = `border-r-${borderColor}-400`;
+    console.log(border)
 	return (
-		<div className="h-screen w-fit border-r-yellow-400 border-r-2 py-4 pl-4 pr-10 flex flex-col sticky top-0 bg-slate-900">
+		<div
+			className={`h-screen w-fit ${border} border-r-2 py-4 pl-4 pr-10 flex flex-col sticky top-0 bg-slate-900`}
+		>
 			<div className="flex flex-col mb-4">
 				<div className="rounded-full border-white border-2 w-20 h-20 flex justify-center items-center">
 					Logo
@@ -11,9 +15,15 @@ export default function Home() {
 				<div className="text-xl">Racusin</div>
 			</div>
 			<div className="flex-grow flex flex-col justify-center items-start space-y-2">
-				<Link href={"/"}>Typography</Link>
-				<Link href={"/albumworks"}>Album Work</Link>
-				<Link href={"/posters"}>Posters</Link>
+				<Link href={"/"} className="hover:text-yellow-400">
+					Typography
+				</Link>
+				<Link href={"/albumworks"} className="hover:text-blue-400">
+					Album Work
+				</Link>
+				<Link href={"/posters"} className="hover:text-red-400">
+					Posters
+				</Link>
 			</div>
 		</div>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Sidebar from "./components/Sidebar";
 export default function Home() {
 	return (
 		<div className="flex flex-row">
-			<Sidebar />
-			<div className="grid grid-cols-3 gap-10 w-full h-fit mt-14 mx-10">
+			<Sidebar borderColor="yellow" />
+			<div className="grid grid-cols-3 gap-10 w-full h-fit my-4 mx-10">
 				<ImagePoster />
 				<ImagePoster />
 				<ImagePoster />
