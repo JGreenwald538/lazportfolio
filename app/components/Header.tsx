99105feaaafcdb1e64b6ea95ad28c16038616ce4
Jack Greenwald <72763093+JGreenwald538@users.noreply.github.com>
2025-01-08 01:55:08 +0000

    Fixed mobile descriptions

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header({name, description}: {name: string, description: 
 	return (
 		<div className="flex md:flex-row flex-col md:h-56 md:mt-0 mt-4 items-center justify-center">
 			<div className="text-4xl font-semibold md:ml-20 ml-0 w-fit">{name}</div>
-			<div className="text-xl md:w-1/2 w-3/4 md:ml-6 ml-0 md:mt-0 mt-2">
+			<div className="text-xl md:w-1/2 w-3/4 md:ml-6 ml-0 md:mt-0 mt-2 md:text-left text-center">
 				{description}
 			</div>
 		</div>
